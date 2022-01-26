8d3c850e9a0d5817382b20d29d86a47691fa905b
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-01-26 23:54:02 +0530

    Create greatest.js

---
+++ 25th jan/greatest.js
@@ -0,0 +1,15 @@
+var a= prompt("enter first number");
+var b= prompt("enter second number");
+var c= prompt("enter third number");
+if(a>=b && a>=c)
+{
+console.log("first number is greatest");
+}
+else if(b>=a && b>=c)
+{
+console.log("second number is greatest");
+}
+else
+{
+console.log("third number is greatest");
+}
