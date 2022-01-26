9956df8c58f13fde0940f10854083a8b0dbb2db6
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-01-27 00:00:10 +0530

    Create smalles.js

---
+++ 26th jan/smalles.js
@@ -0,0 +1,15 @@
+var a= prompt("enter first number");
+var b= prompt("enter second number");
+var c= prompt("enter third number");
+if(a<=b && a<=c)
+{
+console.log("first number is smallest");
+}
+else if(b<=a && b<=c)
+{
+console.log("second number is smallest");
+}
+else
+{
+console.log("third number is smallest");
+}
