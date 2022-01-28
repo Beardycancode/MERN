a3e9db2d10be09aaabee3266c8796bf34d25ecc1
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-01-28 20:02:14 +0530

    Create maxarr.js
    1) Find the size of array from user
    2) Take data of array from user
    3) Run a for loop {for(i=0; i<a; i++)}
    4) First value will be assigned as max value and our we will check wether it is max or not by if{if(arr[i]>max} condition
    5) If its not max..the loop will conitnue for the element bigger than arr[0] 
    6) after comparing all values  we will get max value

---
+++ 27th jan/maxarr.js
@@ -0,0 +1,21 @@
+var a=prompt("Enter total values ");
+var arr=[];
+var i=0;
+
+for(i=0; i<a; i++){
+    arr[i]=prompt("Enter data");
+}
+    console.log(arr);
+
+var max=arr[0];
+
+var i=0;
+
+  for(i=0; i<a; i++){
+      if(arr[i]>max){
+          max=arr[i];
+     }
+}
+
+alert(max +" is your maximum value");
+console.log(max);
