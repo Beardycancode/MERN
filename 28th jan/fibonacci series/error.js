f2845a3928c49121eb2bc822b5d127be7f76c677
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-01-29 20:51:59 +0530

    Update error.js

--- 28th jan/fibonacci series/error.js
+++ 28th jan/fibonacci series/error.js
@@ -4,11 +4,16 @@ var n2=1,n,i;
 var f=prompt("Enter limit of series");
 f=parseInt(f);
 
-console.log("fibonacci series:");
+if(isNaN(n))
+{
+    alert("enter valid number")
+}
+  console.log("fibonacci series:");
 
 for(i=1;i<=f;i++)
 {
-console.log(n1);
+
+  console.log(n1);
 n=n1+n2;
 n1=n2;
 n2=n;
