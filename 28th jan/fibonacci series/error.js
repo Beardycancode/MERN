9455fa6de79c9635cebfde534c15733c5f2a26a3
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-01-29 17:54:40 +0530

    Create error.js

---
+++ 28th jan/fibonacci series/error.js
@@ -0,0 +1,15 @@
+var n1=0;
+var n2=1,n,i;
+
+var f=prompt("Enter limit of series");
+f=parseInt(f);
+
+console.log("fibonacci series:");
+
+for(i=1;i<=f;i++)
+{
+console.log(n1);
+n=n1+n2;
+n1=n2;
+n2=n;
+}
