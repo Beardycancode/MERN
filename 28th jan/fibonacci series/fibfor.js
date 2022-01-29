937fbcfafdf62239b8c6721082c4622eab3d4aa0
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-01-29 20:52:51 +0530

    Update fibfor.js

--- 28th jan/fibonacci series/fibfor.js
+++ 28th jan/fibonacci series/fibfor.js
@@ -2,11 +2,17 @@ var n1=0;
 var n2=1,n;
 var f=prompt("Enter limit of series");
 f=parseInt(f);
-document.write("fibonacci series:");
+
+if(isNaN(n))
+{
+ alert("enter valid number")
+}
+  document.write("fibonacci series:");
+
 for(i=1;i<=f;i++)
 {
-document.write(n1);
-n=n1+n2;
-n1=n2;
-n2=n;
+  document.write(n1);
+  n=n1+n2;
+  n1=n2; 
+  n2=n;
 }
