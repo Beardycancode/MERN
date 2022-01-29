f130e0e9fc27a2eaac08735f80bac5d75d155508
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-01-29 17:50:34 +0530

    Create fibfor.js

---
+++ 28th jan/fibonacci series/fibfor.js
@@ -0,0 +1,12 @@
+var n1=0;
+var n2=1,n;
+var f=prompt("Enter limit of series");
+f=parseInt(f);
+document.write("fibonacci series:");
+for(i=1;i<=f;i++)
+{
+document.write(n1);
+n=n1+n2;
+n1=n2;
+n2=n;
+}
