e50577bb5a29efef82ed8abc96dd4feb73299508
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-02-06 17:02:38 +0530

    Update fibfor.js

--- 28th jan/fibonacci series/fibfor.js
+++ 28th jan/fibonacci series/fibfor.js
@@ -3,12 +3,6 @@ var n2=1,n;
 var f=prompt("Enter limit of series");
 f=parseInt(f);
 
-if(isNaN(n))
-{
- alert("enter valid number")
- 
-}
-else 
 document.write("fibonacci series:");
 
 for(i=1;i<=f;i++)
