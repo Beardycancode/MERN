ac7ca70e0d1e1d75558985c0040be19b41d10922
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-02-06 16:54:59 +0530

    Update fibwhile.js

--- 28th jan/fibonacci series/fibwhile.js
+++ 28th jan/fibonacci series/fibwhile.js
@@ -2,13 +2,13 @@
 var f=prompt("enter limit");
 f=parseInt(f);
 var n1=0,n2=1,n3;
-document.write((n1);
-document.write((n2);
+console.log((n1);
+console.log((n2);
  n3= n1+n2;
 
 while(n3<=f)
 {
- document.write(n3);
+ console.log(n3);
  n1=n2;
  n2=n3;
  n3=n1+n2;
