c27b526057d4d547b19920bec489e046e3d6f861
Yashvardhan Singh <96344411+Beardycancode@users.noreply.github.com>
2022-02-06 17:09:51 +0530

    Update prime.js

--- 29th jan/prime.js
+++ 29th jan/prime.js
@@ -1,12 +1,6 @@
 var n = prompt("enter your number");
 n=parseInt(n);
-
-if(isNaN(n))
-{
-  alert("enter valid number");
- 
-  else
-  {
+   { 
     for(i=2;i<=n-1;i++)
     if(n%i==0){
     flag=false;
@@ -16,5 +10,4 @@ if(isNaN(n))
   alert("it is prime");
   else
   alert("it is composite");
-}
   
